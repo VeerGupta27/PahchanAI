5f52889942a0691720196df11b4faaa069a3be25
Samriddhi422 <samriddhi1422@gmail.com>
2026-03-11 18:52:22 +0530

    register

--- Backend/Models/UserModel.js
+++ Backend/Models/UserModel.js
@@ -1,21 +1,27 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-
-    phone: {
-      type: String,
-    },
-
- 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true,
-  }
-);
 
-export default mongoose.model("User", UserSchema);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- Backend/Server.js
+++ Backend/Server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use("/users", userRoutes);
 app.get("/testdb", async (req, res) => {
   const data = await mongoose.connection.db.collection("test").insertOne({
     name: "PahchanAI",
