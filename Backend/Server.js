1d57dec97feb19316cb136759b219d5acb25c7ee
Samriddhi422 <samriddhi1422@gmail.com>
2026-03-11 18:27:03 +0530

    res

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,9 +13,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.get('/',(res,req)=>{
-    res.send("running bacend")
-})
+
 connectDB();
 
 app.listen(port, () => console.log("server started " + port));
