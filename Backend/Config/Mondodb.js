ac8995595a81dd89ff0492f670b40f2bec22a637
Samriddhi422 <samriddhi1422@gmail.com>
2026-03-11 17:46:19 +0530

    bacend

--- Backend/Config/Mondodb.js
+++ Backend/Config/Mondodb.js
@@ -1,12 +1,11 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      family: 4,
-    });
-    console.log("MongoDB Connected (Direct Mode) ✅");
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("MongoDB Connected ✅");
+
   } catch (err) {
     console.error("Connection Error:", err.message);
   }
--- Backend/Server.js
+++ Backend/Server.js
@@ -1,16 +1,19 @@
-import dns from 'node:dns';
-dns.setDefaultResultOrder('ipv4first');
-import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './Config/Mondodb.js';
+import dns from "node:dns";
+dns.setDefaultResultOrder("ipv4first");
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./Config/Mondodb.js";
+
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-connectDB()
-app.listen(port,()=>console.log("server started" + port));
+connectDB();
+
+app.listen(port, () => console.log("server started " + port));
