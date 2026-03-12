d5eee2cea78d125dc55006e88db46ddae23a6632
VeerGupta27 <veer27bc067@satiengg.in>
2026-03-12 08:51:28 +0530

    alert

--- Backend/Routes/suspectRoutes.js
+++ Backend/Routes/suspectRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { addSuspect, getAllSuspects } from "../Controllers/suspectController.js";
 import upload from "../middleware/uploads.js";
+import { createAlert } from "../Controllers/alertController.js";
 console.log("Suspect routes loaded");
 const routerSuspect = express.Router();
 
 routerSuspect.post("/add-suspect", upload.single("image"), addSuspect);
 routerSuspect.get("/get-missing" , getAllSuspects)
+routerSuspect.post("/alert" , createAlert)
+
 routerSuspect.get("/test", (req,res)=>{
   res.send("AI route working");
 });
