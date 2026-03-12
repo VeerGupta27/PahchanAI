1c68a4668aa7c74da328b4cf0b9f6dc63bd514cd
Samriddhi422 <samriddhi1422@gmail.com>
2026-03-13 02:16:21 +0530

    api

--- ai-service/api.py
+++ ai-service/api.py
@@ -27,27 +27,19 @@ async def create_embedding(file: UploadFile = File(...)):
 
     image_path = os.path.join(TEMP_DIR, person_id + ".jpg")
 
-    # save uploaded image
     with open(image_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # generate embedding
-    embedding_path = generate_embedding(image_path, person_id)
+    embedding = generate_embedding(image_path)
 
-    if embedding_path is None:
+    if embedding is None:
         return {
             "success": False,
-            "message": "No face detected in image"
+            "message": "No face detected"
         }
-
-    embedding_file = os.path.basename(embedding_path)
-
-    # full public URL
-    embedding_url = f"https://pahchanai-1.onrender.com/embeddings/{embedding_file}"
 
     return {
         "success": True,
         "person_id": person_id,
-        "embedding_file": embedding_file,
-        "embedding_url": embedding_url
+        "embedding": embedding
     }
