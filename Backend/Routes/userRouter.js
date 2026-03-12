17c47257d2091f6731ae9d09a9f0b9ff66c8a3a5
VeerGupta27 <veer27012005@gmail.com>
2026-03-12 23:52:35 +0530

    l

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, registerUser } from "../Controllers/userController.js";
+import { loginUser, registerUser } from "../Controllers/userControllers.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
