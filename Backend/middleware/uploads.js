63a27fe3ca18c07405c6cdf831d346f73f069d08
VeerGupta27 <veer27bc067@satiengg.in>
2026-03-12 05:42:42 +0530

    upload folder

--- Backend/middleware/uploads.js
+++ Backend/middleware/uploads.js
@@ -1,8 +1,16 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadDir = "uploads";
+
+// create uploads folder if not exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
